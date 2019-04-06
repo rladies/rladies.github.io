f6d50b6993e12218941dac98e922b4755a12007d
Bea Hernández <bea@Eriador.local>
2019-04-06 13:26:08 +0200

    api key netlify

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo "The TRAVIS_PULL_REQUEST_BRANCH is $TRAVIS_PULL_REQUEST_BRANCH"
 if [[ "$TRAVIS_PULL_REQUEST_BRANCH" = "" ]]
 then
 	zip -r r-ladies.zip public/
-	curl -X POST -H "Content-Type: application/zip" -H "Authorization: Bearer $NETLIFYKEY" --data-binary "@r-ladies.zip" https://api.netlify.com/api/v1/sites/c2e9da14-7780-4e3d-8a57-b0d3c8287762/deploys
+	curl -X POST -H "Content-Type: application/zip" -H "Authorization: Bearer $NETLIFYKEY" --data-binary "@r-ladies.zip" https://api.netlify.com/api/v1/sites/728e663f-18fb-432c-80d6-840c043ce50a/deploys
 else
     echo "You are not on master, deploying preview."
 fi
