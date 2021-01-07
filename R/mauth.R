6e820420ee3e932752c2d1551f002ca44b093820
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2021-01-07 22:27:45 +0100

    fix auth

--- R/mauth.R
+++ R/mauth.R
@@ -1,14 +1,14 @@
 key <- cyphr::key_sodium(sodium::hex2bin(Sys.getenv("MEETUPR_PWD")))
 temptoken <- tempfile(fileext = ".rds")
 cyphr::decrypt_file(
-  testthat::test_path("R/secret.rds"),
+  here::here("R/secret.rds"),
   key = key,
   dest = temptoken
 )
 
 token <- readRDS(temptoken)[[1]]
 token <- meetupr::meetup_auth(
-  token = token,
+  token = temptoken,
   set_renv = FALSE,
   cache = FALSE
 ) 
