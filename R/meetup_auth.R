3e6f224eda735354fb6e33be005ab41269940dc5
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2021-01-16 18:51:38 +0100

    more flexible and with less rquests to meetup

---
+++ R/meetup_auth.R
@@ -0,0 +1,21 @@
+cat("Session is non-interactive. Starting meetup authentication process.\n")
+
+key <- cyphr::key_sodium(sodium::hex2bin(Sys.getenv("MEETUPR_PWD")))
+temptoken <- tempfile(fileext = ".rds")
+cyphr::decrypt_file(
+  here::here("R/secret.rds"),
+  key = key,
+  dest = temptoken
+)
+
+token <- readRDS(temptoken)[[1]]
+token <- meetup_auth(
+  token = temptoken,
+  set_renv = FALSE,
+  cache = FALSE
+) 
+
+Sys.setenv(MEETUPR_PAT = temptoken)
+
+cat("\t authenticating...\n\n")
+k <- meetup_auth(token = temptoken)
