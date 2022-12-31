632f994bcab82f02182143ef8d5ae3ff71233cce
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2022-12-31 22:54:26 +0100

    fix json schema GT

--- scripts/validate_jsons.R
+++ scripts/validate_jsons.R
@@ -1,21 +1,37 @@
 validate_jsons <- function(files, schema){
   validate <- jsonvalidate::json_validator(schema)
-
-  k <- sapply(files, 
-              validate,
-              verbose = TRUE, 
-              error = FALSE, 
-              greedy = TRUE)
-
   
-  if(any(!k)){
-    k <- k[!k]
-    errs <- attr(k, "errors", TRUE)
-    stop(
-      "Some jsons are not formatted correctly\n",
-      paste(" ", errs$field, errs$message, "\n"),
-      call. = FALSE
+  .val <- function(x){
+    z <- tryCatch(validate(x,
+                           verbose = TRUE,
+                           error = FALSE,
+                           greedy = TRUE),
+                  error = function(e) e)
+    
+    msg <- NA
+    
+    if(!is.logical(z)){
+      msg <- sprintf("json error: '%s'", z$message)
+      z <- FALSE
+    }else if(!z){
+      errs <- attr(z, "errors", TRUE)
+      msg <- sprintf("'%s' %s",  errs$field, errs$message)
+    }
+    list(success = z,
+         msg = sprintf("%s: %s", basename(x), msg) |>
+           paste(collapse = "\n")
     )
+  }
+  k <- lapply(files, .val)
+  names(k) <- basename(files)
+  
+  idx <- which(!sapply(k, function(x) x$success))
+  j <- sapply(idx, function(x) k[[x]]$msg)
+  
+  if(length(j) > 0){
+    cat(paste(j, collapse = "\n"))
+    cat("\n")
+    stop("json validation failed", call. = FALSE)
   }
 }
 
