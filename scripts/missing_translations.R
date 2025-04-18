d213841a1555ade320b05613f35580cdbbea1d2a
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2025-04-18 23:11:40 +0200

    init try babeldown

--- scripts/missing_translations.R
+++ scripts/missing_translations.R
@@ -1,71 +1,120 @@
-# Get all mds
-# Find all non-post content
-library(here)
-content <- list.files(here("content"),"index", 
-                      recursive = TRUE, 
-                      full.names = TRUE)
-content <- content[grepl("[.]md", content)]
+#' Automatically translate a text file using DeepL API.
+#'
+#' @description
+#' The `autotranslate` function translates a specified text file from the source
+#' language to the target language using the `babeldown::deepl_translate_hugo`
+#' function. It processes YAML fields for metadata translation, adds custom
+#' YAML fields to indicate the translation status, and saves the translated file.
+#'
+#' @param to_lang A string specifying the target language (e.g., "en", "fr").
+#' @param from_lang A string specifying the source language (e.g., "en", "de").
+#' @param file A string specifying the file path of the text file to translate.
+#'
+#' @return
+#' Nothing is returned. The translated file is saved to disk as a new file,
+#' with the target language in its name.
+#'
+#' @examples
+#' # Translate a file from English to French:
+#' autotranslate("fr", "en", "posts/my_post.en.md")
+#'
+#' # Translate a file from German to English:
+#' autotranslate("en", "de", "posts/article.de.md")
+#'
+#' @export
+autotranslate <- function(to_lang, from_lang, file) {
+  cat("Translating", file, "to", to_lang, "\n ")
 
-# Get the unique directories
-dirs <-  unique(dirname(content))
+  aeolus::unleash(file)
+
+  deepl <- babeldown::deepl_translate_hugo(
+    post_path = file,
+    yaml_fields = c("title", "description", "summary"),
+    glossary_name = NULL,
+    source_lang = from_lang,
+    target_lang = to_lang,
+    formality = "default",
+  )
+
+  # Add custom yaml to indicate auto translation
+  deepl <- c(
+    "---",
+    sprintf("language: %s", to_lang),
+    sprintf("translated: no"),
+    deepl[2:(length(deepl))]
+  )
+
+  new_file <- gsub(
+    sprintf("[.]%s[.]", from_lang),
+    sprintf(".%s.", to_lang),
+    file
+  )
+
+  writeLines(deepl, new_file)
+}
+
+# Get all index mds
+
+Sys.setenv(DEEPL_API_KEY = "8e104762-81f7-422c-ac2f-c71239bf973d:fx")
+
+index_files <- list.files(
+  here("content/news"),
+  "^index",
+  recursive = TRUE,
+  full.names = TRUE
+)
+index_files <- index_files[grepl("[.]md", index_files)]
+
+bundles <- unique(dirname(index_files))
 
 # Get the site languages
 site_lang <- readLines(here("config/_default/languages.yaml"))
 site_lang <- gsub(":", "", site_lang[grep("^[a-z]", site_lang)])
-cat("Populating untranslated files:\n ")
+
 # Loop through dirs
-for(k in dirs){
-  tmp <- content[grepl(k, content)]
-  tmp_files <- basename(tmp)
-  
-  j <- sapply(site_lang, function(x) grepl(x, tmp_files))
-  if(!is.null(dim(j)))
-    j <- apply(j, 2, any)
-  j <- j[!j]
-  
-  orig_file <- tmp[1]
-  orig_cont <- readLines(orig_file)
-  orig_lang <- sapply(sprintf("\\.%s\\.", site_lang), grepl, x = orig_file)
-  orig_lang <- site_lang[orig_lang]
-  
-  idx <- grep("---", orig_cont)[2]
-  yaml <- orig_cont[1:idx]
-  yaml <- c(yaml[1:(length(yaml)-1)], 
-            "translated: no", 
-            yaml[length(yaml)])
-  yaml <- c(yaml[1:(length(yaml)-1)], 
-            paste("language:", orig_lang), 
-            yaml[length(yaml)])
-  
-  for(lang in names(j)){
-    new_file <- gsub(sprintf("[.]%s[.]", orig_lang), 
-                     sprintf(".%s.", lang), 
-                     orig_file)
-    yaml2 <- gsub(sprintf("language: %s", orig_lang), 
-                  sprintf("language: %s", lang), 
-                  yaml)
-    new_cont <- c(yaml2, orig_cont[-1:-idx], "")
-    cat(new_file, "'\n")
-    writeLines(new_cont, new_file)
+for (content in bundles) {
+  bundle_index <- index_files[grepl(content, index_files)]
+  source_file <- bundle_index[1]
+
+  j <- sapply(site_lang, function(x) {
+    grepl(paste0(".", x, ".md$"), bundle_index)
+  })
+  if (!is.null(dim(j))) j <- apply(j, 2, any)
+  if (all(j == FALSE)) {
+    j["en"] <- TRUE
   }
+  orig_lang <- names(j[j])[1]
+
+  if (!grepl(sprintf("[.]%s[.]md", orig_lang), source_file)) {
+    tmpf <- source_file
+    source_file <- sprintf(
+      "%s.%s.md",
+      tools::file_path_sans_ext(source_file),
+      orig_lang
+    )
+    file.rename(tmpf, source_file)
+  }
+
+  lapply(
+    names(j[!j]),
+    autotranslate,
+    from_lang = orig_lang,
+    file = source_file
+  )
 }
 
 # If you've generated non-translated content to
-# check out how it looks. Delete them again by 
+# check out how it looks. Delete them again by
 # running this function.
-delete_non_translates <- function(){
+delete_autotranslated <- function() {
   # Find all non-post content
-  content <- list.files("content","index", 
-                        recursive = TRUE, 
-                        full.names = TRUE)
-  yaml <- lapply(content, 
-                 readLines, 
-                 15)
-  
-  idx <- which(sapply(yaml, function(x){
+  content <- list.files("content", "index", recursive = TRUE, full.names = TRUE)
+  yaml <- lapply(content, readLines, 15)
+
+  idx <- which(sapply(yaml, function(x) {
     any(grepl("translated: no", x))
   }))
-  
+
   sapply(content[idx], file.remove)
 }
-# delete_non_translates()
+# delete_autotranslated()
